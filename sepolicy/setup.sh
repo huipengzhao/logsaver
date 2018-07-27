d67413c5a32f90b0ece391d3fbfb17df46ec92e7
zhaohp <zhaohp@thundersoft.com>
2018-07-27 15:03:13 +0800

    Refactor sepolicy directory.

--- sepolicy/setup.sh
+++ sepolicy/setup.sh
@@ -2,10 +2,10 @@
 
 set -x
 
-cp private_logsaver.te ${ANDROID_BUILD_TOP}/system/sepolicy/private/logsaver.te
+cp private/logsaver.te ${ANDROID_BUILD_TOP}/system/sepolicy/private/logsaver.te
 
-cat private_file_contexts >> ${ANDROID_BUILD_TOP}/system/sepolicy/private/file_contexts
+cat private/file_contexts >> ${ANDROID_BUILD_TOP}/system/sepolicy/private/file_contexts
 
-cat private_compat_26.0_26.0.ignore.cil >> ${ANDROID_BUILD_TOP}/system/sepolicy/private/compat/26.0/26.0.ignore.cil
+cat private/compat/26.0/26.0.ignore.cil >> ${ANDROID_BUILD_TOP}/system/sepolicy/private/compat/26.0/26.0.ignore.cil
 
 set +x
